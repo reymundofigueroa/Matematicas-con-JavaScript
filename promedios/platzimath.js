cc10d943f6f7ba8b3b413483b344bcd73aba66e1
Reymundo <reymundo_124@hotmail.com>
2024-06-09 21:59:34 -0600

    Seccion de estadistica: avanses de promedio y mediana

--- promedios/platzimath.js
+++ promedios/platzimath.js
@@ -1,3 +1,4 @@
+// Calcular promedio
 
 function calcPromedio(arr){
   let sumaArr = 0;
@@ -13,6 +14,8 @@ function calcPromedio(arr){
 
 calcPromedio([10,10,10,10,10,9])
 
+// Par o inpar
+
 function unpair(arr){
   if (arr.length % 2 == 1){
     console.log(arr.length % 2)
@@ -26,20 +29,31 @@ function unpair(arr){
 
 unpair([1,1])
  
+// Mediana de un arreglo 
+
 const pair = (arr) => {return !(arr.length % 2)};
 
-function mediana(arr){
-  const listaPar = pair(arr)
+function mediana(unsortedList){
+  const lista = sortList(unsortedList)
+  const listaPar = pair(lista)
   if (listaPar){
-    const indexMedianaLista1 = Math.floor(arr.length / 2);
-    const indexMedianaLista2 = Math.floor((arr.length / 2) - 1);
-    const medianaPar = ((arr[indexMedianaLista1] + arr[indexMedianaLista2]) / 2);
+    const indexMedianaLista1 = Math.floor(lista.length / 2);
+    const indexMedianaLista2 = Math.floor((lista.length / 2) - 1);
+    const medianaPar = ((lista[indexMedianaLista1] + lista[indexMedianaLista2]) / 2);
     console.log(medianaPar)
   } else {
-    const indexMedianaLista = Math.floor(arr.length / 2);
-    const medianaInpar = arr[indexMedianaLista];
+    const indexMedianaLista = Math.floor(lista.length / 2);
+    const medianaInpar = lista[indexMedianaLista];
     console.log(medianaInpar);
   }
 }
 
-mediana([209,5200,500]);
+mediana([209,5200,500,111]);
+
+// Ordenamiento sort
+function sortList (unsortedList){
+  const list = unsortedList.sort(ListaSort = (acumulatedValue, currentValue) => acumulatedValue - currentValue);
+  return list;
+};
+
+console.log(sortList([10,20,1,2,3,50]));
